0b74f0d66c958382fc6284cc2d75a9fb624b0230
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-12-13 14:23:51 +0100

    init

---
+++ lua/ayoub/set.lua
@@ -0,0 +1,47 @@
+vim.o.hidden = true
+vim.o.splitbelow = true
+vim.o.splitright = true
+
+
+vim.opt.nu = true
+vim.opt.relativenumber = true
+
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/local/share/nvim/undodir"
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
+
+vim.opt.termguicolors = true
+vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.updatetime = 50
+vim.opt.colorcolumn = "80"
+vim.g.mapleader = " "
+
+
+vim.cmd [[
+
+set spellsuggest+=5
+set linebreak
+set linespace=5
+set clipboard+=unnamedplus
+set clipboard+=unnamed
+set inccommand=split
+
+
+set nowrap
+set autoindent
+set mouse=a
+]]
