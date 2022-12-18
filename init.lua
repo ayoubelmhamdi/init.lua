1b06b6776e01bad4babf19fe4bb7e18b28a99677
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2022-12-18 21:21:09 +0100

    up

--- init.lua
+++ init.lua
@@ -2,8 +2,13 @@ require 'ayoub'
 
 vim.keymap.set('n', '<space>s', function()
   package.loaded.gg = nil
-  vim.cmd 'w'
-  require 'gg'
+  -- vim.cmd 'w'
+  R 'gg'
+  require('gg').view_function()
+end, {})
+
+vim.keymap.set('n', '<space>e', function()
+  require('gg').view_function()
 end, {})
 
 require('Comment').setup {}
