251dd1c822782c4eb07206f5abd115ba12956afa
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-03-25 01:05:15 -0100

    (tree-sitter.lua) disable auto_install and wait to fix the bug behind it that not allow open new filetype

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -14,10 +14,17 @@ return {
     require('nvim-treesitter.configs').setup {
       ensure_installed = { 'help', 'javascript', 'typescript', 'c', 'lua', 'rust' },
       sync_install = false,
-      auto_install = true,
+      auto_install = false,
       highlight = {
         enable = true,
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
       },
     }
   end,
