7c82d4bcd6f2fc37a9529aeb69ac7afd19462a99
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-04-04 23:13:38 -0100

    (bind.lua) rename a funcation

--- lua/ayoub/bind.lua
+++ lua/ayoub/bind.lua
@@ -1,34 +1,34 @@
 local M = {}
 
-M.defualt_opts = { noremap = true, silent = true }
+M.default_opts = { noremap = true, silent = true }
 
 M.n = function(lhs, rhs, opts)
-  opts = opts or M.defualt_opts
+  opts = opts or M.default_opts
   vim.keymap.set('n', lhs, rhs, opts)
 end
 
 M.v = function(lhs, rhs, opts)
-  opts = opts or M.defualt_opts
+  opts = opts or M.default_opts
   vim.keymap.set('v', lhs, rhs, opts)
 end
 
 M.i = function(lhs, rhs, opts)
-  opts = opts or M.defualt_opts
+  opts = opts or M.default_opts
   vim.keymap.set('i', lhs, rhs, opts)
 end
 
 M.x = function(lhs, rhs, opts)
-  opts = opts or M.defualt_opts
+  opts = opts or M.default_opts
   vim.keymap.set('x', lhs, rhs, opts)
 end
 
 M.o = function(lhs, rhs, opts)
-  opts = opts or M.defualt_opts
+  opts = opts or M.default_opts
   vim.keymap.set('o', lhs, rhs, opts)
 end
 
 M.t = function(lhs, rhs, opts)
-  opts = opts or M.defualt_opts
+  opts = opts or M.default_opts
   vim.keymap.set('t', lhs, rhs, opts)
 end
 
