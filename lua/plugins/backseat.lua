16d9f2ea41ff51d91290525cb5fbe08d099af771
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-04-04 23:13:16 -0100

    (backseat.lua) add backset

---
+++ lua/plugins/backseat.lua
@@ -0,0 +1,8 @@
+return {
+    "james1236/backseat.nvim",
+    config = function()
+        require("backseat").setup({
+            openai_model_id = 'gpt-3.5-turbo', --gpt-4
+        })
+    end
+}
