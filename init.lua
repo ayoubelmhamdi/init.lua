408f1144d6f968e75a3b6eae80d9be8e3a177ad0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-04-09 07:21:26 -0100

    (init.lua) change vf.nvim name to correct one

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 require 'ayoub'
 
 vim.keymap.set('n', '<space>s', function()
-  -- package.loaded.gg = nil
+  package.loaded.gg = nil
   -- vim.cmd 'w'
-  R 'gg'
-  require('gg').view_function()
+  R 'vf'
+  require('vf').view_function()
 end, {})
 
 vim.keymap.set('n', '<space>e', function()
-  require('gg').view_function()
+  require('vf').view_function()
 end, {})
 --
 -- Create an autocommand group
