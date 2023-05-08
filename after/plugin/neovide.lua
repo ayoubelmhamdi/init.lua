4b546d13e6c5fc9500db482d204d1e60a7821f22
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-08 23:04:35 +0100

    (neovide.lua) change font

--- after/plugin/neovide.lua
+++ after/plugin/neovide.lua
@@ -4,6 +4,6 @@ if exists("g:neovide")
     let g:neovide_cursor_animation_length=0.02
     let g:neovide_cursor_antialiasing=v:true
     let g:neovide_cursor_vfx_mode = "ripple"
-    set guifont=Fisa\ Code:h20
+    set guifont=Cascadia\ Code:h14
 endif
 ]]
