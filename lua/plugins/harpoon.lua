f49762a285f5909844dfb5a88f087ea96b73acd1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-08 23:06:54 +0100

    (harpoon.lua) many tchange for kebinding

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,20 +1,21 @@
-return { -- file switcher
+return {
+  -- file switcher
   'ThePrimeagen/harpoon',
   lazy = true,
   keys = {
-    { '<leader>a', '<CMD> lua require("harpoon.mark").add_file() <CR>', desc = 'add current file' },
-    { '<C-e>', '<CMD> lua require("harpoon.ui").toggle_quick_menu() <CR>' },
-    { '<leader>1', '<CMD> lua require("harpoon.ui").nav_file(1) <CR>' },
-    { '<leader>2', '<CMD> lua require("harpoon.ui").nav_file(2) <CR>' },
-    { '<leader>3', '<CMD> lua require("harpoon.ui").nav_file(3) <CR>' },
-    { '<leader>4', '<CMD> lua require("harpoon.ui").nav_file(4) <CR>' },
-    { '<leader>at', '<CMD> vsplit lua require("harpoon.mark").get_marked_file(1).filename <CR>' },
+    { '<C-e>', '<CMD> lua require("harpoon.ui").toggle_quick_menu()<CR>' },
+    { '<leader>a', '<CMD> lua require("harpoon.mark").add_file()<CR>' },
+
+    { '<leader>1', '<CMD> lua require("harpoon.ui").nav_file(1)<CR>' },
+    { '<leader>2', '<CMD> lua require("harpoon.ui").nav_file(2)<CR>' },
+    { '<leader>3', '<CMD> lua require("harpoon.ui").nav_file(3)<CR>' },
+    { '<leader>4', '<CMD> lua require("harpoon.ui").nav_file(4)<CR>' },
+
+    { '<leader>n', ' <cmd>:lua require("harpoon.ui").nav_next()<cr>' },
+    { '<leader>p', ' <cmd>:lua require("harpoon.ui").nav_prev()<cr>' },
   },
   dependencies = {
     'nvim-telescope/telescope.nvim',
     'nvim-lua/plenary.nvim',
   },
-  config = function()
-    require('telescope').load_extension 'harpoon'
-  end,
 }
