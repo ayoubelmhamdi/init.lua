90ee746bac21c1e6bf4cf76953892751f26816ae
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-08 23:06:37 +0100

    (distance.lua) disable ditance

--- lua/plugins/distance.lua
+++ lua/plugins/distance.lua
@@ -1,12 +1,13 @@
 return {
-  'chipsenkbeil/distant.nvim',
-  -- version = '0.1.2',
-  dependencies = {
-    { 'nvim-telescope/telescope-fzf-native.nvim' },
-  },
-  config = function()
-    require('distant').setup {
-      ['*'] = require('distant.settings').chip_default(),
-    }
-  end,
 }
+--   'chipsenkbeil/distant.nvim',
+--   -- version = '0.1.2',
+--   dependencies = {
+--     { 'nvim-telescope/telescope-fzf-native.nvim' },
+--   },
+--   config = function()
+--     require('distant').setup {
+--       ['*'] = require('distant.settings').chip_default(),
+--     }
+--   end,
+-- }
