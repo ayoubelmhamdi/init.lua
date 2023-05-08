1eed187ada509d96ce43047fee3af6a512994032
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-08 23:09:31 +0100

    (typst.lua) one of best typst plugin syntax

--- lua/plugins/typst.lua
+++ lua/plugins/typst.lua
@@ -1,5 +1,11 @@
 return {
-  'kaarmu/typst.vim',
-  ft = 'typ',
-  lazy=false,
+  {
+    'kaarmu/typst.vim',
+    ft = 'typst',
+    lazy = false,
+  },
+  -- {
+  --   'SeniorMars/typst.nvim',
+  -- },
+  {},
 }
