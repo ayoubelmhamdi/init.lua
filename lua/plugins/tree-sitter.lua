ac801b84434e255dbe3f27c73894da668021d8e1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-14 20:32:59 +0100

    (tree-sitter.lua) disable markdown now at fixing later

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -17,14 +17,15 @@ return {
       auto_install = false,
       highlight = {
         enable = true,
+        disable = { "markdown" },
         additional_vim_regex_highlighting = true,
-        disable = function(lang, buf)
-          local max_filesize = 100 * 1024 -- 100 KB
-          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
-          if ok and stats and stats.size > max_filesize then
-            return true
-          end
-        end,
+        -- disable = function(lang, buf)
+        --   local max_filesize = 100 * 1024 -- 100 KB
+        --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+        --   if ok and stats and stats.size > max_filesize then
+        --     return true
+        --   end
+        -- end,
       },
     }
     local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
