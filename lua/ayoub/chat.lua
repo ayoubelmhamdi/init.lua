3cfca00aa463976e0a8486046be8224599debeab
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-05-15 02:14:01 +0100

    (chat.lua) add support to visual mode

--- lua/ayoub/chat.lua
+++ lua/ayoub/chat.lua
@@ -1,22 +1,16 @@
-local CHAT = {}
+local M = {}
 
-function CHAT.ask(engine)
-  if not engine then
-    vim.api.nvim_buf_set_lines(0, -1, -1, false, { '---no-engine----' })
-    return
-  end
+function M.ask(engine, all_lines)
+  local text = table.concat(all_lines, '\n')
 
-  local all_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
-  all_lines = table.concat(all_lines, '\n')
-
-  local command = { engine, '-m', all_lines }
+  local command = { engine, '-m', text }
   local append_data = function(_, data)
     if data then
       vim.api.nvim_buf_set_lines(0, -1, -1, false, data)
     end
   end
 
-  vim.api.nvim_buf_set_lines(0, -1, -1, false, { '---' .. engine .. '----' })
+  vim.api.nvim_buf_set_lines(0, -1, -1, false, { '# ---' .. engine .. '----' })
   vim.fn.jobstart(command, {
     stdout_buffered = true,
     on_stdout = append_data,
@@ -24,4 +18,30 @@ function CHAT.ask(engine)
   })
 end
 
-return CHAT
+M.main = function(engine, mode)
+  if not engine then
+    vim.api.nvim_buf_set_lines(0, -1, -1, false, { '# ---no-engine----' })
+    return
+  end
+
+  NORMAL_MODE = mode
+
+  local all_lines = {}
+  if NORMAL_MODE then
+    all_lines = vim.api.nvim_buf_get_lines(0, 0, -1, true)
+  else
+    all_lines = M.nvim_get_selected_text()
+  end
+  M.ask(engine, all_lines)
+end
+
+M.nvim_get_selected_text = function()
+  local bufnr = vim.api.nvim_get_current_buf()
+  -- get start/end of visual selection
+  local start_row, start_col = unpack(vim.api.nvim_buf_get_mark(bufnr, '<'))
+  local end_row, end_col = unpack(vim.api.nvim_buf_get_mark(bufnr, '>'))
+  local text = vim.api.nvim_buf_get_text(bufnr, start_row - 1, start_col, end_row - 1, end_col + 1, {})
+  return text or {}
+end
+
+return M
