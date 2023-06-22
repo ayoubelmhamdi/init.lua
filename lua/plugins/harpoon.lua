65ef84788d13be37514b6aa2ad633ecbb3daa050
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-06-22 17:34:37 +0100

    (harpoon.lua) improve

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -3,16 +3,16 @@ return {
   'ThePrimeagen/harpoon',
   lazy = true,
   keys = {
-    { '<C-e>', '<CMD> lua require("harpoon.ui").toggle_quick_menu()<CR>' },
-    { '<leader>a', '<CMD> lua require("harpoon.mark").add_file()<CR>' },
+    { '<c-e>', '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>' },
+    { '<leader>a', '<cmd>lua require("harpoon.mark").add_file()<cr>' },
 
-    { '<leader>1', '<CMD> lua require("harpoon.ui").nav_file(1)<CR>' },
-    { '<leader>2', '<CMD> lua require("harpoon.ui").nav_file(2)<CR>' },
-    { '<leader>3', '<CMD> lua require("harpoon.ui").nav_file(3)<CR>' },
-    { '<leader>4', '<CMD> lua require("harpoon.ui").nav_file(4)<CR>' },
+    { '<leader>1', '<cmd>lua require("harpoon.ui").nav_file(1)<cr>' },
+    { '<leader>2', '<cmd>lua require("harpoon.ui").nav_file(2)<cr>' },
+    { '<leader>3', '<cmd>lua require("harpoon.ui").nav_file(3)<cr>' },
+    { '<leader>4', '<cmd>lua require("harpoon.ui").nav_file(4)<cr>' },
 
-    { '<leader>n', ' <cmd>:lua require("harpoon.ui").nav_next()<cr>' },
-    { '<leader>p', ' <cmd>:lua require("harpoon.ui").nav_prev()<cr>' },
+    { '<leader>n', ' <cmd>lua require("harpoon.ui").nav_next()<cr>' },
+    { '<leader>p', ' <cmd>lua require("harpoon.ui").nav_prev()<cr>' },
   },
   dependencies = {
     'nvim-telescope/telescope.nvim',
