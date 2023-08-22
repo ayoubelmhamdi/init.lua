8f99ac0d8e9b98d3da172521033b6737770dbdc0
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-08-22 02:21:04 +0100

    (backseat.lua) + autocmd

--- lua/plugins/backseat.lua
+++ lua/plugins/backseat.lua
@@ -1,8 +1,9 @@
 return {
-    "james1236/backseat.nvim",
-    config = function()
-        require("backseat").setup({
-            openai_model_id = 'gpt-3.5-turbo', --gpt-4
-        })
-    end
+  'james1236/backseat.nvim',
+  cmd = 'Backseat',
+  config = function()
+    require('backseat').setup {
+      openai_model_id = 'gpt-3.5-turbo', --gpt-4
+    }
+  end,
 }
