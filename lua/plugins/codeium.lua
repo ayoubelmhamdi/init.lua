a4527c44d9e3c751f568b315033993a6fed1e81e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-08-22 02:37:04 +0100

    (codeium.lua) make it lazy and for some filetype

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,18 +1,16 @@
 return {
+  'jcdickinson/codeium.nvim',
+  event = 'VeryLazy',
+  ft = {'dart', 'c','python'},
+  dependencies = {
     {
-        "jcdickinson/http.nvim",
-        build = "cargo build --workspace --release"
+      'jcdickinson/http.nvim',
+      build = 'cargo build --workspace --release',
     },
-    {
-        "jcdickinson/codeium.nvim",
-        dependencies = {
-            "jcdickinson/http.nvim",
-            "nvim-lua/plenary.nvim",
-            "hrsh7th/nvim-cmp",
-        },
-        -- config = function()
-        --     require("codeium").setup({
-        --     })
-        -- end
-    }
+    'nvim-lua/plenary.nvim',
+    'hrsh7th/nvim-cmp',
+  },
+  config = function()
+    require('codeium').setup {}
+  end,
 }
