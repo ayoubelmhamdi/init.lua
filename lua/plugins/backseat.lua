a5d6e66c725e0a497bf016e7afb218fde83469ee
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-08-22 02:23:02 +0100

    (backseat.lua) active the plugin when this all cmd asked

--- lua/plugins/backseat.lua
+++ lua/plugins/backseat.lua
@@ -1,6 +1,6 @@
 return {
   'james1236/backseat.nvim',
-  cmd = 'Backseat',
+  cmd = {'Backseat','BackseatAsk','BackseatClear','BackseatClearLine'},
   config = function()
     require('backseat').setup {
       openai_model_id = 'gpt-3.5-turbo', --gpt-4
