81ee4cb1b825e6a5596b0a701824d25b98349912
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-18 02:31:59 +0100

    (wezterm.lua) i do not have time to memorize weztem.lua

--- lua/plugins/wezterm.lua
+++ lua/plugins/wezterm.lua
@@ -1,7 +1,7 @@
 return {
-  'willothy/wezterm.nvim',
-  cmd = 'WeztermSpawn',
-  opts = {
-    create_commands = true,
-  },
+  -- 'willothy/wezterm.nvim',
+  -- cmd = 'WeztermSpawn',
+  -- opts = {
+  --   create_commands = true,
+  -- },
 }
