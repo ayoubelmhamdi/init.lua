2fa3085393cea7526797d40474da536edb859158
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-10-18 02:29:38 +0100

    (codeium.lua) diasble for now

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -10,7 +10,7 @@ return {
     'nvim-lua/plenary.nvim',
     'hrsh7th/nvim-cmp',
   },
-  config = function()
-    require('codeium').setup {}
-  end,
+  -- config = function()
+  --   require('codeium').setup {}
+  -- end,
 }
