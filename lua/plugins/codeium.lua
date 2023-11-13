a394f6ed904d16ec284c519e417fd927a61ac5d8
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-11-14 00:43:17 +0100

    (codeium.lua) i don't use cmp now

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -1,16 +1,30 @@
 return {
-  'jcdickinson/codeium.nvim',
+  -- 'jcdickinson/codeium.nvim',
+  'Exafunction/codeium.vim',
   event = 'VeryLazy',
-  ft = {'dart', 'c','python'},
-  dependencies = {
-    {
-      'jcdickinson/http.nvim',
-      build = 'cargo build --workspace --release',
-    },
-    'nvim-lua/plenary.nvim',
-    'hrsh7th/nvim-cmp',
-  },
-  -- config = function()
-  --   require('codeium').setup {}
-  -- end,
+  ft = { 'dart', 'c', 'python' },
+  -- dependencies = {
+  --   {
+  --     'jcdickinson/http.nvim',
+  --     build = 'cargo build --workspace --release',
+  --   },
+  --   'nvim-lua/plenary.nvim',
+  --   'hrsh7th/nvim-cmp',
+  -- },
+  config = function()
+    -- require('codeium').setup {}
+    -- Change '<C-g>' here to any keycode you like.
+    vim.keymap.set('i', '<C-g>', function()
+      return vim.fn['codeium#Accept']()
+    end, { expr = true })
+    vim.keymap.set('i', '<c-;>', function()
+      return vim.fn['codeium#CycleCompletions'](1)
+    end, { expr = true })
+    vim.keymap.set('i', '<c-,>', function()
+      return vim.fn['codeium#CycleCompletions'](-1)
+    end, { expr = true })
+    vim.keymap.set('i', '<c-x>', function()
+      return vim.fn['codeium#Clear']()
+    end, { expr = true })
+  end,
 }
