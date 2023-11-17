dcfdfb7e92781bc29ebe16b86577c8dfef685f3e
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-11-17 18:56:42 +0100

    up

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -12,18 +12,18 @@ return {
   --   'hrsh7th/nvim-cmp',
   -- },
   config = function()
-    -- require('codeium').setup {}
-    -- Change '<C-g>' here to any keycode you like.
-    vim.keymap.set('i', '<C-g>', function()
+    vim.g.codeium_disable_bindings = 1
+
+    vim.keymap.set('i', '<C-e>', function()
       return vim.fn['codeium#Accept']()
     end, { expr = true })
-    vim.keymap.set('i', '<c-;>', function()
+    vim.keymap.set('i', '<c-j>', function()
       return vim.fn['codeium#CycleCompletions'](1)
     end, { expr = true })
-    vim.keymap.set('i', '<c-,>', function()
+    vim.keymap.set('i', '<c-k>', function()
       return vim.fn['codeium#CycleCompletions'](-1)
     end, { expr = true })
-    vim.keymap.set('i', '<c-x>', function()
+    vim.keymap.set('i', '<c-y>', function()
       return vim.fn['codeium#Clear']()
     end, { expr = true })
   end,
