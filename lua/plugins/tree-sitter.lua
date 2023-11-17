25adc85809a507370276a62f2924fedf85bea5d6
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-11-17 18:57:20 +0100

    (tree-sitter.lua) to use add syntax like conceal we should active vim regix

--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -13,7 +13,7 @@ return {
   --
   config = function()
     require('nvim-treesitter.configs').setup {
-      ensure_installed = { 'markdown', 'markdown_inline', 'javascript', 'typescript', 'c', 'lua', 'rust' },
+      ensure_installed = { 'markdown', 'markdown_inline', 'javascript', 'typescript', 'c', 'python', 'lua', 'rust' },
       sync_install = false,
       auto_install = false,
       -- 'filNaj/tree-setter'
@@ -24,7 +24,7 @@ return {
       highlight = {
         enable = true,
         -- disable = { 'markdown' },
-        additional_vim_regex_highlighting = false,
+        additional_vim_regex_highlighting = true,
         -- disable = function(lang, buf)
         --   local max_filesize = 100 * 1024 -- 100 KB
         --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
