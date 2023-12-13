3932c1cdd79f65691d542d1573d6ac22b39e69fd
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-12-13 23:52:59 +0100

    up

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -16,9 +16,13 @@ return {
         },
         { 'leoluz/nvim-dap-go' },
         {
+            'LiadOz/nvim-dap-repl-highlights',
+            config = function() require('nvim-dap-repl-highlights').setup() end,
+        },
+        {
             'rcarriga/nvim-dap-ui',
             keys = {
-                { 'v', '<A-e>', '<cmd>lua require("dapui").eval()<CR>', { noremap = true, silent = true } },
+                -- { '<A-e>', { '<cmd>lua require("dapui").eval()<CR>', mode = 'v' }, { noremap = true, silent = true } },
             },
             config = function()
                 vim.fn.sign_define('DapBreakpoint', { text = '🦦', texthl = '', linehl = '', numhl = '' })
@@ -41,12 +45,12 @@ return {
     },
     keys = {
         { '<leader>db', ":lua require'dap'.toggle_breakpoint()<cr>", desc = 'debug breakpoint' },
-        { '<leader>dc', ":lua require'dap'.continue()<cr>", desc = 'debug' },
-        { '<leader>dn', ":lua require'dap'.step_over()<cr>", desc = 'debug next' },
-        { '<leader>ds', ":lua require'dap'.step_into()<cr>", desc = 'debug into' },
-        { '<leader>do', ":lua require'dap'.step_out()<cr>", desc = 'debug out' },
-        { '<leader>dr', ":lua require'dap'.repl_open()<cr>", desc = 'debug repl' },
-        { '<leader>du', ":lua require'dapui'.toggle()<cr>", desc = 'debug ui' },
+        { '<leader>dc', ":lua require'dap'.continue()<cr>",          desc = 'debug' },
+        { '<leader>dn', ":lua require'dap'.step_over()<cr>",         desc = 'debug next' },
+        { '<leader>ds', ":lua require'dap'.step_into()<cr>",         desc = 'debug into' },
+        { '<leader>do', ":lua require'dap'.step_out()<cr>",          desc = 'debug out' },
+        { '<leader>dr', ":lua require'dap'.repl_open()<cr>",         desc = 'debug repl' },
+        { '<leader>du', ":lua require'dapui'.toggle()<cr>",          desc = 'debug ui' },
     },
     config = function()
         require('dap').configurations.python = {
