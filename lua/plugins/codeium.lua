ca0bfe1443329a6593e4770d636d936bc7988a55
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-12-15 03:18:05 +0100

    up

--- lua/plugins/codeium.lua
+++ lua/plugins/codeium.lua
@@ -14,7 +14,7 @@ return {
   config = function()
     vim.g.codeium_disable_bindings = 1
 
-    vim.keymap.set('i', '<TAB>', function() return vim.fn['codeium#Accept']() end, { expr = true })
+    -- vim.keymap.set('i', '<C-b>', function() return vim.fn['codeium#Accept']() end, { expr = true })
     vim.keymap.set('i', '<C-e>', function() return vim.fn['codeium#Accept']() end, { expr = true })
     vim.keymap.set('i', '<c-j>', function() return vim.fn['codeium#CycleCompletions'](1) end, { expr = true })
     vim.keymap.set('i', '<c-k>', function() return vim.fn['codeium#CycleCompletions'](-1) end, { expr = true })
