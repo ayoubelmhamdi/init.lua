57f0aefbc6e8518db24e93982329a1e29244448a
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-12-15 03:18:01 +0100

    up

--- lua/ayoub/set.lua
+++ lua/ayoub/set.lua
@@ -7,13 +7,17 @@ vim.g.markdown_fenced_languages = { 'html', 'python', 'bash', 'sh' }
 opt.completeopt = { 'menuone' }
 
 opt.cursorline = true
+opt.shiftround = true
+opt.timeoutlen = 300 -- time to wait for a mapped sequence to complete (in milliseconds)
+opt.path:append('**') -- search down into subfolders
+opt.breakindent = true -- visually wrap lines
 
 -- Ignore compiled files
 opt.wildignore = '__pycache__'
-opt.wildignore:append { '*.o', '*~', '*.pyc', '*pycache*' }
-opt.wildignore:append { 'Cargo.lock', 'Cargo.Bazel.lock' }
+opt.wildignore:append({ '*.o', '*~', '*.pyc', '*pycache*' })
+opt.wildignore:append({ 'Cargo.lock', 'Cargo.Bazel.lock' })
 
-opt.pumblend = 7
+opt.pumblend = 30
 opt.wildmode = 'longest:full'
 opt.wildoptions = 'pum'
 
@@ -37,15 +41,24 @@ opt.softtabstop = 4
 opt.shiftwidth = 4
 opt.expandtab = true
 
-opt.smartindent = true
+opt.formatoptions = opt.formatoptions
+    - 'a' -- Auto formatting is BAD.
+    - 't' -- Don't auto format my code. I got linters for that.
+    + 'c' -- In general, I like it when comments respect textwidth
+    + 'q' -- Allow formatting comments w/ gq
+    - 'o' -- O and o, don't continue comments
+    - 'r' -- Don't insert comment after <Enter>
+    + 'n' -- Indent past the formatlistpat, not underneath it.
+    + 'j' -- Auto-remove comments if possible.
+    - '2' -- I'm not in gradeschool anymore
 
 opt.wrap = false
 
 local tmp_undo = ''
-if os.getenv 'TMPDIR' then
-  tmp_undo = os.getenv 'TMPDIR' .. '/nvim/undodir'
+if os.getenv('TMPDIR') then
+    tmp_undo = os.getenv('TMPDIR') .. '/nvim/undodir'
 else
-  tmp_undo = '/tmp/nvim/undodir'
+    tmp_undo = '/tmp/nvim/undodir'
 end
 opt.swapfile = false
 opt.backup = false
@@ -56,11 +69,13 @@ opt.hlsearch = true
 opt.incsearch = true
 opt.showmatch = true
 opt.ignorecase = true -- Ignore case when searching...
-opt.smartcase = true
+opt.smartcase = true -- override ignorecase if search pattern contains upper case characters
+opt.smarttab = true
+opt.smartindent = true
 
 opt.termguicolors = true
 opt.signcolumn = 'yes'
-opt.isfname:append '@-@'
+opt.isfname:append('@-@')
 opt.updatetime = 300
 opt.colorcolumn = '80'
 
@@ -71,8 +86,7 @@ opt.wrap = false
 opt.autoindent = true
 opt.mouse = 'a'
 
-opt.clipboard = 'unnamedplus'
-opt.clipboard:append { 'unnamed' }
+opt.clipboard = 'unnamedplus,unnamed'
 
 opt.inccommand = 'split'
 opt.spellsuggest = { 'best', 6 }
