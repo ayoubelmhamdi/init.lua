e642f3e004887bd0da2ca2ab1169e2ce35ea9322
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-01-19 22:31:33 +0100

    up

--- lua/plugins/dap.lua
+++ lua/plugins/dap.lua
@@ -6,11 +6,15 @@ return {
     dependencies = {
         { 'leoluz/nvim-dap-go' },
         {
+            'LiadOz/nvim-dap-repl-highlights',
+            config = function() require('nvim-dap-repl-highlights').setup() end,
+        },
+        {
             'Weissle/persistent-breakpoints.nvim',
             config = function() require('persistent-breakpoints').setup({ load_breakpoints_event = { 'BufReadPost' } }) end,
             event = 'BufReadPost',
             keys = {
-                { '<leader>db', ":lua require'persistent-breakpoints.api'.toggle_breakpoint()<cr>", desc = 'debug breakpoint' },
+                { '<leader>db',  ":lua require'persistent-breakpoints.api'.toggle_breakpoint()<cr>",         desc = 'debug breakpoint' },
                 { '<Leader>dbc', ":lua require'persistent-breakpoints.api'.set_conditional_breakpoint()<cr>" },
             },
         },
@@ -41,8 +45,8 @@ return {
                 require('dapui').setup({
                     icons = { expanded = '▾', collapsed = '▸' },
                     layouts = {
-                        { elements = { 'scopes', 'breakpoints', 'watches' }, size = 40, position = 'left' },
-                        { elements = { 'repl' }, size = 10, position = 'bottom' },
+                        { elements = { 'scopes', 'breakpoints', 'repl' }, size = 40, position = 'left' },
+                        { elements = { 'watches', 'console' },            size = 10, position = 'bottom' },
                     },
                 })
             end,
@@ -57,27 +61,40 @@ return {
         },
     },
     keys = {
-        { '<leader><space>', ":lua require'dap'.step_over()<cr>", desc = 'debug next' },
-        { '<leader>dc', ":lua require'dap'.continue()<cr>", desc = 'debug' },
-        { '<leader>dn', ":lua require'dap'.step_over()<cr>", desc = 'debug next' },
-        { '<leader>ds', ":lua require'dap'.step_into()<cr>", desc = 'debug into' },
-        { '<leader>do', ":lua require'dap'.step_out()<cr>", desc = 'debug out' },
-        { '<leader>dr', ":lua require'dap'.restart()<cr>", desc = 'debug repl' },
-        { '<leader>dro', ":lua require'dap'.repl_open()<cr>", desc = 'debug repl' },
-        { '<leader>du', ":lua require'dapui'.toggle()<cr>", desc = 'debug ui' },
-        { '<Leader>dhh', ":lua require('dap.ui.variables').hover()<CR>" },
-        { '<Leader>dhv', ":lua require('dap.ui.variables').visual_hover()<CR>" }, --v
+        { '<leader><space>', ":lua require'dap'.step_over()<cr>zz",                                                    desc = 'debug next' },
+        { '<leader>dc',      ":lua require'dap'.continue()<cr>",                                                       desc = 'debug' },
+        { '<leader>dn',      ":lua require'dap'.step_over()<cr>",                                                      desc = 'debug next' },
+        { '<leader>ds',      ":lua require'dap'.step_into()<cr>",                                                      desc = 'debug into' },
+        { '<leader>do',      ":lua require'dap'.step_out()<cr>",                                                       desc = 'debug out' },
+        { '<leader>dr',      ":lua require'dap'.restart()<cr>",                                                        desc = 'debug repl' },
+        { '<leader>dro',     ":lua require'dap'.repl_open()<cr>",                                                      desc = 'debug repl' },
+        { '<leader>du',      ":lua require'dapui'.toggle()<cr>",                                                       desc = 'debug ui' },
+        { '<Leader>dhh',     ":lua require('dap.ui.variables').hover()<CR>" },
+        { '<Leader>dhv',     ":lua require('dap.ui.variables').visual_hover()<CR>" }, --v
 
-        { '<Leader>duf', ":lua local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes)<CR>" },
+        { '<Leader>duf',     ":lua local widgets=require('dap.ui.widgets');widgets.centered_float(widgets.scopes)<CR>" },
     },
     config = function()
         local dap = require('dap')
         local dapui = require('dapui')
         local api = vim.api
-
+        dap.defaults.fallback.exception_breakpoints = { 'raised' }
         dap.listeners.after.event_initialized['dapui_config'] = function() dapui.open() end
 
-        dap.configurations.python = { { type = 'python', request = 'launch', name = 'Launch file', program = '${file}' } }
+        -- require('dap.ext.vscode').load_launchjs(nil, { cppdbg = {'c', 'cpp'} })
+        require('dap.ext.vscode').load_launchjs()
+        -- dap.configurations.python = {
+        --     {
+        --         type = 'python',
+        --         request = 'launch',
+        --         name = 'Launch file',
+        --         program = '${file}',
+        --         cwd = vim.fn.getcwd(),
+        --         justMyCode = false,
+        --         console = 'integratedTerminal',
+        --         autoReload = { enbale = true },
+        --     },
+        -- }
 
         local keymap_restore = {} --{{{
         dap.listeners.after['event_initialized']['me'] = function()
@@ -95,7 +112,8 @@ return {
 
         dap.listeners.after['event_terminated']['me'] = function()
             for _, keymap in pairs(keymap_restore) do
-                api.nvim_buf_set_keymap(keymap.buffer, keymap.mode, keymap.lhs, keymap.rhs, { silent = keymap.silent == 1 })
+                api.nvim_buf_set_keymap(keymap.buffer, keymap.mode, keymap.lhs, keymap.rhs,
+                    { silent = keymap.silent == 1 })
             end
             keymap_restore = {}
         end
