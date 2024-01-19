cb18e8315c42503a963482e4cc8a9795504e6b68
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-01-19 22:31:47 +0100

    up

--- init.lua
+++ init.lua
@@ -2,3 +2,17 @@ require('ayoub.set')
 require('ayoub.remap')
 require('ayoub.autocmd')
 require('ayoub.lazy')
+
+vim.g.asmsyntax = 'nasm'
+-- Disable Default Vim Plugins
+vim.g.loaded_gzip = 0
+vim.g.loaded_tar = 0
+vim.g.loaded_tarPlugin = 0
+vim.g.loaded_zipPlugin = 0
+vim.g.loaded_2html_plugin = 0
+vim.g.loaded_netrw = 0
+vim.g.loaded_netrwPlugin = 0
+vim.g.loaded_spec = 0
+vim.g.loaded_syncolor = 0
+
+
