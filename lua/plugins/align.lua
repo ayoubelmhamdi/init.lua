918bb10bdd6f2b5d8015e4ee5722de03228267ec
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-01-29 02:43:26 +0100

    (align.lua) any lenght

--- lua/plugins/align.lua
+++ lua/plugins/align.lua
@@ -7,7 +7,7 @@ return {
             require('align').align_to_char({
                 regex = true,
                 preview = false,
-                length = 2,
+                length = 10,
             })
         end, { noremap = true, silent = true })
     end,
