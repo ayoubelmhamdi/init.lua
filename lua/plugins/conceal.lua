15f8e19c2070f5ba28cc0c6dd4007ccfb81f203c
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-02-29 18:02:39 +0100

    up

--- lua/plugins/conceal.lua
+++ lua/plugins/conceal.lua
@@ -1,4 +1,8 @@
 return {
-    'khzaw/vim-conceal',
-    -- event = 'VeryLazy',
+    -- if we don't use tiletype, the event=verylazy/lazy=true, triggle off the plugin.
+    {
+        'khzaw/vim-conceal',
+        ft = { 'python', 'ocamel', 'ruby' },
+    },
+    { 'MrPicklePinosaur/typst-conceal.vim', ft = 'typst' },
 }
