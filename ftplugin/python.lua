831fd12ac0096423ce5327e3a51938a49a2c0146
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-03-08 20:10:45 +0100

    up

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -4,3 +4,5 @@ local opt = { noremap = true, silent = true }
 key('n', '<C-f>', "<cmd>lua require('ayoub.diff').toggle({ 'ruff', 'format', vim.fn.expand('%:p'), '--diff' }, 3)<cr>", opt)
 key('n', '<C-c>', "<cmd>lua require('ayoub.diff').toggle({ 'ruff', 'check', vim.fn.expand('%:p'), '--diff' }, 3)<cr>", opt)
 
+key('v', '<C-f>', "<cmd>lua require('ayoub.diff').toggle({ 'echo', '-e', 'Hell\\nWord'}, 2)<cr>", opt)
+
