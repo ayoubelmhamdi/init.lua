b005308ec7711bdbd81da65554cc8539185ede7a
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-03-08 17:46:48 +0100

    (cmp.lua) cmp need at least one snippet tool like luasnip

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -7,7 +7,8 @@ return {
         { 'hrsh7th/cmp-cmdline' },
         { 'hrsh7th/cmp-path' },
         { 'hrsh7th/cmp-buffer' },
-        -- { 'saadparwaiz1/cmp_luasnip' },
+        { 'saadparwaiz1/cmp_luasnip' },
+        { 'L3MON4D3/LuaSnip' },
         { 'hrsh7th/cmp-nvim-lsp' },
         -- { 'hrsh7th/cmp-nvim-lua' },
         { 'hrsh7th/cmp-nvim-lsp-signature-help' },
@@ -138,6 +139,7 @@ return {
         }
         ---------------------------- CMP SETUP -------------------------------------
         cmp.setup({
+            snippet = { expand = function(args) require('luasnip').lsp_expand(args.body) end },
             sources = sources,
             formatting = formatting,
             mapping = mapping,
