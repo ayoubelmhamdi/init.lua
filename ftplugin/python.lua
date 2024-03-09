9e70da206579393e97cd79b778757e369f8434a1
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-03-09 09:34:27 +0100

    (python.lua) test lua to build keybinding

--- ftplugin/python.lua
+++ ftplugin/python.lua
@@ -1,8 +1,20 @@
 local key = vim.keymap.set
 local opt = { noremap = true, silent = true }
 
-key('n', '<C-f>', "<cmd>lua require('ayoub.diff').toggle({ 'ruff', 'format', vim.fn.expand('%:p'), '--diff' }, 3)<cr>", opt)
-key('n', '<C-c>', "<cmd>lua require('ayoub.diff').toggle({ 'ruff', 'check', vim.fn.expand('%:p'), '--diff' }, 3)<cr>", opt)
+local ayoub = require('ayoub.diff')
 
-key('v', '<C-f>', "<cmd>lua require('ayoub.diff').toggle({ 'echo', '-e', 'Hell\\nWord'}, 2)<cr>", opt)
+local kinds = {
+    ['full'] = 1,
+    ['range'] = 2,
+    ['diff'] = 3,
+}
 
+local commands = {
+    ['ruff_format'] = { 'ruff', 'format', vim.fn.expand('%:p'), '--diff' },
+    ['format_check'] = { 'ruff', 'check', vim.fn.expand('%:p'), '--diff' },
+    ['echo'] = { 'echo', '-e', 'Hell\\nWord' },
+}
+
+key('n', '<C-f>', function() ayoub.toggle(commands['ruff_format'], kinds['diff']) end, opt)
+key('n', '<C-c>', function() ayoub.toggle(commands['ruff_check'], kinds['diff']) end, opt)
+key('v', '<C-f>', function() ayoub.toggle(commands['echo'], kinds['range']) end, opt)
