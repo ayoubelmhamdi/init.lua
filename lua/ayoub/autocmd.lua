c65a24344f7f28db9da5b879087263643fee56ad
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2024-04-19 10:03:45 +0100

    up

--- lua/ayoub/autocmd.lua
+++ lua/ayoub/autocmd.lua
@@ -1,12 +1,12 @@
 vim.cmd([[
 
-" augroup restore_pos |
-"   au!
-"   au BufReadPost *
-"       \ if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
-"       \ |   exe "normal! g`\"zz"
-"       \ | endif
-" augroup end
+augroup restore_pos |
+  au!
+  au BufReadPost *
+      \ if line("'\"") >= 1 && line("'\"") <= line("$") && &ft !~# 'commit'
+      \ |   exe "normal! g`\"zz"
+      \ | endif
+augroup end
 
 
 augroup General
