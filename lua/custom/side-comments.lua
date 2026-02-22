656232c4fc888b915d4f846324e2d511cf21f011
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2026-02-22 15:18:19 +0000

    (side-comments.lua) more tweeking

--- lua/custom/side-comments.lua
+++ lua/custom/side-comments.lua
@@ -13,23 +13,59 @@ function M.setup(opts)
   opts = opts or {}
   _G.conceal_langs = vim.tbl_extend("force", conceal_defaults, _G.conceal_langs or {}, opts)
 
-  vim.opt.concealcursor = "nv"
+  local group = vim.api.nvim_create_augroup("SideComments", { clear = true })
+
 
   vim.api.nvim_create_autocmd("InsertEnter", {
+    group = group,
     callback = function()
-      vim.opt.conceallevel = 0
+      if conceal_active[vim.api.nvim_get_current_buf()] then
+        vim.wo.conceallevel = 0
+      end
     end,
   })
 
   vim.api.nvim_create_autocmd("InsertLeave", {
+    group = group,
     callback = function()
       if conceal_active[vim.api.nvim_get_current_buf()] then
-        vim.opt.conceallevel = 2
+        vim.wo.conceallevel = 2
       end
     end,
   })
 
   vim.keymap.set("n", "<leader>tc", M.toggle, { desc = "Toggle conceal prefix" })
+
+  vim.api.nvim_create_autocmd("FileType", {
+    group = group,
+    callback = function()
+      local ft = vim.bo.filetype
+      if (_G.conceal_langs or {})[ft] then
+        vim.wo.concealcursor = "nv"
+        vim.schedule(function()
+          M.toggle_side_comments(true)
+        end)
+      end
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("WinEnter", {
+    group = group,
+    callback = function()
+      local buf = vim.api.nvim_get_current_buf()
+      if conceal_active[buf] then
+        vim.wo.conceallevel = 2
+        vim.wo.concealcursor = "nv"
+      end
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("BufDelete", {
+    group = group,
+    callback = function(args)
+      conceal_active[args.buf] = nil
+    end,
+  })
 end
 
 local function get_n()
@@ -42,13 +78,18 @@ function M.toggle()
 
   if conceal_active[buf] then
     vim.cmd("syntax clear HidePrefix")
-    vim.opt.conceallevel = 0
+    vim.wo.conceallevel = 0
     conceal_active[buf] = nil
   else
     local n = get_n()
+    if n == 0 then
+      vim.notify("No conceal width for filetype: " .. (vim.bo.filetype or "none"), vim.log.levels.WARN)
+      return
+    end
     local dots = string.rep(".", n)
     vim.cmd(string.format([[syntax match HidePrefix /^%s/ conceal cchar=+]], dots))
-    vim.opt.conceallevel = 2
+    vim.wo.conceallevel = 2
+    vim.wo.concealcursor = "nv"
     conceal_active[buf] = true
   end
 end
@@ -59,13 +100,18 @@ function M.toggle_side_comments(enable)
 
   if enable then
     local n = get_n()
+    if n == 0 then
+      vim.notify("No conceal width for filetype: " .. (vim.bo.filetype or "none"), vim.log.levels.WARN)
+      return
+    end
     local dots = string.rep(".", n)
     vim.cmd(string.format([[syntax match HidePrefix /^%s/ conceal cchar=+]], dots))
-    vim.opt.conceallevel = 2
+    vim.wo.conceallevel = 2
+    vim.wo.concealcursor = "nv"
     conceal_active[buf] = true
   else
     vim.cmd("syntax clear HidePrefix")
-    vim.opt.conceallevel = 0
+    vim.wo.conceallevel = 0
     conceal_active[buf] = nil
   end
 end
