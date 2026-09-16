bf61080abdee6ad9eb00afcbf56fa4706ed0d325
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2026-09-16 20:25:34 +0100

    reenable treesitre

--- lua/custom/treesitter.lua
+++ lua/custom/treesitter.lua
@@ -1,9 +1,9 @@
 local REQ = require('ayoub.mini_functions').REQ
 
-local configs = REQ('nvim-treesitter.configs')
-if not (configs) then return end
+local config = REQ('nvim-treesitter')
+if not (config) then return end
 
-configs.setup({
+config.setup({
     ensure_installed = { 'markdown', 'markdown_inline', 'javascript', 'typescript', 'c', 'python', 'lua', 'rust', 'bash', 'cpp', 'dart', 'gitcommit', 'html', 'json', 'latex', 'query', 'vimdoc', 'xml', 'yaml' },
 
     sync_install = false,
@@ -16,7 +16,7 @@ configs.setup({
     highlight = {
         enable = true,
         -- disable = { 'markdown' },
-        additional_vim_regex_highlighting = true,
+        -- additional_vim_regex_highlighting = false,
         -- disable = function(lang, buf)
         --   local max_filesize = 100 * 1024 -- 100 KB
         --   local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
@@ -27,31 +27,31 @@ configs.setup({
     },
 })
 
--- local parsers = REQ('nvim-treesitter.parsers')
--- local install = REQ('nvim-treesitter.install')
--- if not (parsers and install) then return end
-
--- install.prefer_git = true
--- local parser_config = parsers.get_parser_configs()
--- parser_config.typst = {
---     install_info = {
---         -- url = 'https://github.com/frozolotl/tree-sitter-typst.git',
---         url = '/data/local_tmp/github_treesitters/tree-sitter-typst',
---         files = { 'src/parser.c', 'src/scanner.c' },
---         -- branch = 'main',
---     },
---     filetype = 'typst', -- if filetype does not agrees with parser name
--- }
-
-
-local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
-parser_config.jai = {
-  install_info = {
-    url = "https://github.com/constantitus/tree-sitter-jai",
-    files = { "src/parser.c", "src/scanner.c" },
-    revision = "c61176d276761e6ee44a86b018446a1608b47b99",
-  },
-  filetype = "jai",
-}
-
-
+--2-- local parsers = REQ('nvim-treesitter.parsers')
+--2-- local install = REQ('nvim-treesitter.install')
+--2-- if not (parsers and install) then return end
+--2
+--2-- install.prefer_git = true
+--2-- local parser_config = parsers.get_parser_configs()
+--2-- parser_config.typst = {
+--2--     install_info = {
+--2--         -- url = 'https://github.com/frozolotl/tree-sitter-typst.git',
+--2--         url = '/data/local_tmp/github_treesitters/tree-sitter-typst',
+--2--         files = { 'src/parser.c', 'src/scanner.c' },
+--2--         -- branch = 'main',
+--2--     },
+--2--     filetype = 'typst', -- if filetype does not agrees with parser name
+--2-- }
+--2
+--2
+--2--1 local parser_config = require("nvim-treesitter.parsers").get_parser_configs()
+--2--1 parser_config.jai = {
+--2--1   install_info = {
+--2--1     url = "https://github.com/constantitus/tree-sitter-jai",
+--2--1     files = { "src/parser.c", "src/scanner.c" },
+--2--1     revision = "c61176d276761e6ee44a86b018446a1608b47b99",
+--2--1   },
+--2--1   filetype = "jai",
+--2--1 }
+--2
+--2
--- lua/plugins/tree-sitter.lua
+++ lua/plugins/tree-sitter.lua
@@ -6,12 +6,13 @@ return {
         { 'David-Kunz/treesitter-unit' },
         { 'nvim-treesitter/nvim-treesitter-textobjects' },
         { 'JoosepAlviste/nvim-ts-context-commentstring' },
-        { 'p00f/nvim-ts-rainbow' },
+        -- { 'p00f/nvim-ts-rainbow' },
         -- { 'filNaj/tree-setter' },
     },
     build = ':TSUpdate',
     --
     config = function()
         require('custom.treesitter')
+        vim.treesitter.start()
     end,
 }
